952a3dd769940677c74655f46b4c0b01ac64273b
soongon <soongon@hucloud.co.kr>
2022-05-12 12:23:39 +0900

    initial commit

---
+++ src/main/java/kr/re/kitri/webfluxdemo/configuration/R2DBC_PostgresConnection.java
@@ -0,0 +1,26 @@
+package kr.re.kitri.webfluxdemo.configuration;
+
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+@Configuration
+@EnableR2dbcRepositories
+public class R2DBC_PostgresConnection {
+
+    @Bean
+    public PostgresqlConnectionFactory connectionFactory() {
+        return new PostgresqlConnectionFactory(
+                PostgresqlConnectionConfiguration.builder()
+                        .host("database-1.csebiuu6q8pc.ap-northeast-2.rds.amazonaws.com")
+                        .database("postgres")
+                        .username("postgres")
+                        .password("12345678")
+                        .build()
+                );
+    }
+
+
+}
